fe2592c3fb4f0637f36c5569b0ac4094452b9cc8
yamina guenez <yaminaguenez@gmail.com>
2024-03-28 15:00:22 +0200

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
