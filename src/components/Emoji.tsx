1aa9e18f699164fe5f4390f7e8627b7feb8e5032
yamina guenez <yaminaguenez@gmail.com>
2024-04-18 14:31:02 +0200

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import bullsEye from "../assets/bulls-eye.webp";
+import meh from "../assets/meh.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+
+interface Props {
+  rating: number;
+}
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "25px" },
+  };
+  return <Image {...emojiMap[rating]} boxSize="25px" marginTop={1} />;
+};
+
+export default Emoji;
