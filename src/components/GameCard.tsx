07257d231d9cd7e199b4122a2e135b12880cd42d
yamina guenez <yaminaguenez@gmail.com>
2024-05-28 17:34:03 +0200

    Fetch games details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
@@ -10,7 +11,7 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card >
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <HStack justifyContent={"space-between"} marginBottom={3}>
@@ -20,7 +21,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
         <Heading fontSize="2xl">
-          {game.name}
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
           <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -26,6 +26,9 @@ const axiosInstance= axios.create({
             .post<FetchResponse<T>>(this.endpoint,data)
             .then(res=>res.data);
         }
+        get= (id: number|string)=>{
+            return axiosInstance.get<T>(this.endpoint + '/'+ id).then(res=> res.data);
+        }
     }
     
 export default APIClient;
