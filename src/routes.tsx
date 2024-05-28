26410242f0c38801affdc192cca8d7ec70fa1d5b
yamina guenez <yaminaguenez@gmail.com>
2024-05-28 12:56:40 +0200

    Handling Errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
-import Layout from "./pages/layout";
+import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage/>,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
