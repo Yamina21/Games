9ee49f16a02226c9a7d15f1e1390ef4756bb5492
yamina guenez <yaminaguenez@gmail.com>
2024-05-29 09:21:51 +0200

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
   id: number;
+  genres: Genre[];
+  publishers: Publisher[];
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
